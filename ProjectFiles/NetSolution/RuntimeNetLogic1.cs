7f62b62fb34274993a96f2f344121c66ab22ffe2
reengle <reengle@rockwellautomation.com>
2022-07-19 10:19:25 -0700

    NetLogic

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
